6e65e77ea4c79eb401b00f910866bdba28f76500
DGolf1313 <103085521+DGolf1313@users.noreply.github.com>
2022-08-16 15:58:06 -0500

    24th commit

--- src/main/java/com/promineotech/jeep/controller/JeepOrderController.java
+++ src/main/java/com/promineotech/jeep/controller/JeepOrderController.java
@@ -1,19 +1,12 @@
 package com.promineotech.jeep.controller;
 
-import java.util.List;
-import javax.validation.constraints.Pattern;
-import org.hibernate.validator.constraints.Length;
+import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import com.promineotech.jeep.Constants;
-import com.promineotech.jeep.entity.Jeep;
-import com.promineotech.jeep.entity.JeepModel;
 import com.promineotech.jeep.entity.Order;
 import com.promineotech.jeep.entity.OrderRequest;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -27,14 +20,11 @@ import io.swagger.v3.oas.annotations.servers.Server;
 
 @Validated
 @RequestMapping("/orders")
-@OpenAPIDefinition(info = @Info(
-    title = "Jeep Order Service"), 
-servers = {
-    @Server(url = "http://localhost:8080", 
-        description = "Local server.")})
+@OpenAPIDefinition(info = @Info(title = "Jeep Order Service"),
+    servers = {@Server(url = "http://localhost:8080", description = "Local server.")})
 
 public interface JeepOrderController {
-  
+
   //@// @formatter:off
  
 
@@ -76,7 +66,7 @@ public interface JeepOrderController {
   
   @PostMapping
   @ResponseStatus(code = HttpStatus.CREATED)
-  Order createOrder(@RequestBody OrderRequest orderRequest);
+  Order createOrder(@Valid @RequestBody OrderRequest orderRequest);
 //@formatter:on
 
 }
